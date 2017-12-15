33135fc0c91b0a06418a0882edc56392bc78192f
Sarvesh Singhrour <ssinghrour@gmail.com>
2017-12-16 00:17:37 +0530

    Simple koa app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
@@ -12,3 +12,28 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+const Koa = require('koa');
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time' , `${ms}ms`)
+});
+
+//logger
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+app.use(async ctx => {
+  ctx.body = 'Hello World from Sarvesh';
+});
+
+app.listen(process.env.PORT || 3000);
